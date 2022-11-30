57e65463819819bc5453ead9e8809cffa42ed69c
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-30 16:43:26 +0600

    routes added

---
+++ src/Pages/Products/Products.js
@@ -0,0 +1,179 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { FaCheckCircle } from "react-icons/fa";
+import { AuthContext } from '../Contexts/AuthProvider';
+import ReportModal from '../Modal/ReportModal/ReportModal';
+import BookingModal from '../Modal/BookingModal/BookingModal';
+
+const Products = () => {
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { catagory } = useParams();
+    const [phone, setPhone] = useState();
+    const [location, setLocation] = useState();
+    const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString();
+    const orderTime = `${date} ${time}`
+    const hadleBookNow = (image, productName, price) => {
+        const order = {
+            name: user.displayName,
+            email: user.email,
+            image,
+            productName,
+            price,
+            orderTime,
+            phone,
+            location
+        }
+        fetch(`https://bikeserver.vercel.app/orders`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                navigate('/dashboard/MyOrders')
+                toast.success('Booking Confirmed')
+            })
+    }
+
+
+    // 
+    const [booking, setBooking] = useState(null);
+    const [products, setProducts] = useState([]);
+    const [productEmail, setProductEmail] = useState('');
+    const [veryfied, setVeryfied] = useState({});
+    const [reportProduct, setReportProduct] = useState(null);
+    const closeModal = () => {
+        setReportProduct(null);
+    }
+    useEffect(() => {
+        fetch(`https://bikeserver.vercel.app/catagory/${catagory}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data.data)
+                if (data.data) {
+                    fetch(`https://bikeserver.vercel.app/veryfied/seller/${productEmail}`)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            setVeryfied(data)
+                        })
+                }
+            })
+    }, [catagory, productEmail])
+    console.log(products)
+    const handleReport = product => {
+        console.log("PRODUCT:", product)
+        const order = {
+            user: user?.displayName,
+            email: user?.email,
+            productId: product._id,
+            productName: product.productName,
+            productSellerMail: product.sellerEmail
+        }
+
+        fetch(`https://bikeserver.vercel.app/report-items`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('You  successfully.')
+                console.log(data);
+                setBooking(null);
+            })
+    }
+    // 
+    return (
+        <div className='mt-10'>
+            {
+                products.map(cata =>
+                    <div key={cata._id}
+                        cata={cata}
+                        setProductEmail={setProductEmail}
+                        setReportProduct={setReportProduct}
+                        veryfied={veryfied}
+                        setBooking={setBooking}
+                        className="card my-12 max-w-screen-md mx-auto bg-base-100 shadow-xl">
+                        <figure><img className='cover' src={cata.image} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{cata.productName}</h2>
+                            <div className='grid grid-cols-2'>
+                                <p>Seller Name: {cata.sellerName}</p>
+                                <p>{veryfied?.status === 'Veryfied' ? <FaCheckCircle /> : ''}</p>
+                            </div>
+                            <div className='grid grid-cols-2'>
+                                <p>Phone: {cata.phone}</p>
+                                <p>Location: {cata.location}</p>
+                                <p>Original Price: {cata.originalPrice}</p>
+                                <p>Resale Price: {cata.resalePrice}</p>
+                                <p>Condition: {cata.condition}</p>
+                                <p>Post Time:{cata.postTime}</p>
+                            </div>
+                            <p>{cata.description}</p>
+
+                            <div className="card-actions justify-between">
+                                <label htmlFor="report-modal" onClick={() => setReportProduct(cata)} className='btn btn-red'>Report</label>
+                                <label htmlFor="bookingModal" className="btn btn-primary">Book Now</label>
+                            </div>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="bookingModal" className="modal-toggle" />
+                            <div className="modal">
+                                <div className="modal-box">
+                                    <label className="label"> <span className="label-text">Product Name</span></label>
+                                    <h2 className="input input-bordered w-full max-w-xs flex items-center">{cata.productName}</h2>
+                                    <label className="label"> <span className="label-text">User Name</span></label>
+                                    <p className="input input-bordered w-full max-w-xs flex items-center">{user.displayName}</p>
+                                    <label className="label"> <span className="label-text">User Email</span></label>
+                                    <p className="input input-bordered w-full max-w-xs flex items-center">{user.email}</p>
+                                    <label className="label"> <span className="label-text">Price</span></label>
+                                    <p className="input input-bordered w-full max-w-xs flex items-center">{cata.resalePrice}</p>
+                                    <label className="label"> <span className="label-text">Phone Number</span></label>
+                                    <input onInput={e => setPhone(e.target.value)} className="input input-bordered w-full max-w-xs" type="text" name="phone" id="" />
+                                    <label className="label"> <span className="label-text">Meeting Location</span></label>
+                                    <input onInput={e => setLocation(e.target.value)} className="input input-bordered w-full max-w-xs" type="text" name="location" id="" />
+                                    <div className="modal-action">
+                                        <label className='btn btn-primary' htmlFor="bookingModal" onClick={() => hadleBookNow(cata.image, cata.productName, cata.resalePrice)}>
+                                            Submit
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            {
+                reportProduct && <ReportModal
+                    title={`Are you sure you want to procced?`}
+                    message={`Do want to report ${reportProduct.productName}?`}
+                    successAction={handleReport}
+                    successButtonName="Confirm"
+                    modalData={reportProduct}
+                    closeModal={closeModal}
+                >
+                </ReportModal>
+            }
+            {
+
+                booking &&
+                <BookingModal
+                    booking={booking}
+                    setBooking={setBooking}
+                ></BookingModal>
+            }
+        </div>
+    );
+};
+
+export default Products;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,23 +1,86 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
+import Blog from "../../Blog/Blog";
+import AllBuyer from "../../Dashboard/Admin/AllBuyer";
+import AllSeller from "../../Dashboard/Admin/AllSeller";
+import ReportedProducts from "../../Dashboard/Admin/ReportedProducts";
+import MyOrders from "../../Dashboard/Buyer/MyOrders";
+import AddProduct from "../../Dashboard/Seller/AddProduct/AddProduct";
+import MyProducts from "../../Dashboard/Seller/MyProducts/MyProducts";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+import Home from "../../Home/Home/Home";
+import DashboardLayout from "../../Layout/DashboardLayout";
+import Payment from "../../Payment/Payment/Payment";
+import Products from "../../Products/Products";
+import Login from "../../User/Login/Login";
+import Signup from "../../User/Signup/Signup";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import AdminRoute from "../usersRoute/AdminRoute/AdminRoute";
+import BuyerRoute from "../usersRoute/BuyerRoute/BuyerRoute";
+import SellerRoute from "../usersRoute/SellerRoute/SellerRoute";
 
-const router = createBrowserRouter([
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+
+export const router = createBrowserRouter([
     {
         path: '/',
-        Element: <Main></Main>,
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
                 element: <Home></Home>
-
             },
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+            {
+                path: `/catagory/:catagory`,
+                element: <PrivateRoute><Products></Products></PrivateRoute>,
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardLayout></DashboardLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/dashboard/addproduct',
+                element: <SellerRoute><AddProduct></AddProduct></SellerRoute>
+            },
+            {
+                path: '/dashboard/allseller',
+                element: <AdminRoute><AllSeller></AllSeller></AdminRoute>
+            },
+            {
+                path: '/dashboard/allbuyer',
+                element: <AdminRoute><AllBuyer></AllBuyer></AdminRoute>
+            },
+            {
+                path: '/dashboard/allreportedproducts',
+                element: <AdminRoute><ReportedProducts></ReportedProducts></AdminRoute>
+            },
+            {
+                path: '/dashboard/MyProducts',
+                element: <SellerRoute><MyProducts></MyProducts></SellerRoute>
+            },
+            {
+                path: '/dashboard/MyOrders',
+                element: <BuyerRoute><MyOrders></MyOrders></BuyerRoute>
+            },
+            {
+                path: '/dashboard/MyOrders/payment/:id',
+                element: <Payment></Payment>
             }
         ]
     }
 ])
-export default router;
